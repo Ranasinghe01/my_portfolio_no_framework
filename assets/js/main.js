0906c9995bc8424577fa70742c939f3699eff139
ranasinghe <kasunranasingha1@gmail.com>
2025-02-01 18:55:07 +0530

    thired commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,40 @@ const blurHeader = () => {
                        : header.classList.remove('blur-header')
 }
 window.addEventListener('scroll', blurHeader)
+
+/*===============EMAIL JS================*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //ServiceID - templateID - #form - public key
+    emailjs.sendForm('service_y4xhyvm', 'template_eup2ji7', '#contact-form', 'XOhcZq8I5uYviVrU1')
+        .then(() => {
+            //Show sent message
+            contactMessage.textContent = 'Message Sent Successfully ✅'
+
+            //Remove message after 5 seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //Clear text fields
+            contactForm.reset()
+        }, () => {
+            //show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+
+
+/*===============SHOW SCROLL UP================*/
+
+
+/*===============SCROLL SECTION ACTIVE LINK================*/
+
+
+/*===============SCROLL REVEAL ANIMATION================*/
