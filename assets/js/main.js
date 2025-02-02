5953ec4c245b67b5c9745d54d8d92475e62bd93f
ranasinghe <kasunranasingha1@gmail.com>
2025-02-02 23:43:19 +0530

    forth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,9 +68,47 @@ contactForm.addEventListener('submit', sendEmail)
 
 
 /*===============SHOW SCROLL UP================*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when the scroll is higher than 350 viewport height, and the
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*===============SCROLL SECTION ACTIVE LINK================*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        }else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*===============SCROLL REVEAL ANIMATION================*/
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true  //Animations repeat
+})
+sr.reveal(`.home__data, .experience, .skills, .contact__container`)
+sr.reveal(`.home__img`, {delay: 600})
+sr.reveal(`.home__scroll`, {delay: 800})
+sr.reveal(`.work__card, .services__card`, {interval: 100})
+sr.reveal(`.about__content`, {origin: 'right'})
+sr.reveal(`.about__img`, {origin: 'left'})
